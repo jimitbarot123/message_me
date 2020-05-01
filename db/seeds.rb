b7fce5dda22d750877efea4529ec721ddf386943
Jimit Barot <jimitbarot31@gmail.com>
2020-05-01 16:43:36 +0530

    Added message resource, executed migration and using seed insert the data into tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "Jimit", password: "test1234")
-User.create(username: "Dhara", password: "test1234")
-User.create(username: "Dhaval", password: "test1234")
-User.create(username: "Vikas", password: "test1234")
-User.create(username: "Amit", password: "test1234")
+# User.create(username: "Jimit", password: "test1234")
+# User.create(username: "Dhara", password: "test1234")
+# User.create(username: "Dhaval", password: "test1234")
+# User.create(username: "Vikas", password: "test1234")
+# User.create(username: "Amit", password: "test1234")
+
+Message.create(body: "Hello", user_id: 1)
+Message.create(body: "How are you?", user_id: 2)
+Message.create(body: "I am doing good", user_id: 3)
